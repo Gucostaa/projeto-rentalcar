d0f1ff7bc1a1bb01224e6ace7102feb60a26ef1b
Gustavo Costa <117370951+Gucostaa@users.noreply.github.com>
2024-04-04 22:01:02 -0300

    login and register update

--- rentalcar/register.php
+++ rentalcar/register.php
@@ -11,45 +11,45 @@
 </head>
 <body>
     <div class="container">
-        <form action="" method="post">
+        <form action="register.php" method="post">
             <h2>Registre-se</h2>
             <div class="content">
                 <div class="input-box">
-                    <label for="name">Nome Completo</label>
-                    <input type="text" placeholder="Enter full name" name="name" required>
+                    <label for="nome">Nome Completo</label>
+                    <input type="text" placeholder="Enter full name" name="nome" required>
                 </div>
                 <div class="input-box">
-                    <label for="username">Nome de Usuário</label>
-                    <input type="text" placeholder="Enter username" name="uname" required>
+                    <label for="usuario">Nome de Usuário</label>
+                    <input type="text" placeholder="Enter username" name="usuario" required>
                 </div>
                 <div class="input-box">
                     <label for="email">Email</label>
                     <input type="email" placeholder="Enter your valid email" name="email" required>
                 </div>
                 <div class="input-box">
-                    <label for="phonenumber">Número de Telefone</label>
-                    <input type="tel" placeholder="Enter phone number" name="phoneNumber" required>
+                    <label for="telefone">Número de Telefone</label>
+                    <input type="tel" placeholder="Enter phone number" name="telefone" required>
                 </div>
                 <div class="input-box">
-                    <label for="password">Senha</label>
-                    <input type="password" placeholder="Enter new password" name="password" required>
+                    <label for="senha">Senha</label>
+                    <input type="password" placeholder="Enter new password" name="senha" required>
                 </div>
                 <div class="input-box">
-                    <label for="Cpassword">Confirmar Senha</label>
-                    <input type="password" placeholder="Confirm password" name="confirmPassword" required>
+                    <label for="csenha">Confirmar Senha</label>
+                    <input type="password" placeholder="Confirm password" name="csenha" required>
                 </div>
                 <span class="gender-title">Genero</span>
                 <div class="gender-category">
-                    <input type="radio" name="gender" id="male">
-                    <label for="gender">Masculino</label>
-                    <input type="radio" name="gender" id="female">
-                    <label for="gender">Feminino</label>
-                    <input type="radio" name="gender" id="other">
-                    <label for="gender">Outro</label>
+                    <input type="radio" name="genero" id="male" value="Masculino">
+                    <label for="male">Masculino</label>
+                    <input type="radio" name="genero" id="female" value="Feminino">
+                    <label for="female">Feminino</label>
+                    <input type="radio" name="genero" id="other" value="Outro">
+                    <label for="other">Outro</label>
                 </div>
             </div>
             <div class="alert">
-                <p>By clicking Sing Up, you agree to our <a href="#">Terms</a>, <a href="#">Privacy Policy</a> and <a href="#">Cookies Policy</a>, You may receive SMS notifications from us and can opt out at any time.</p>
+                <p>By clicking Sign Up, you agree to our <a href="#">Terms</a>, <a href="#">Privacy Policy</a> and <a href="#">Cookies Policy</a>, You may receive SMS notifications from us and can opt out at any time.</p>
             </div>
             <div class="button-container">
                 <button type="submit">Registre-se</button>
@@ -58,3 +58,49 @@
     </div>
 </body>
 </html>
+
+<?php
+include 'dbconnect.php';
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Receber dados do formulário
+    $nome = $_POST['nome'];
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $senha = $_POST['senha'];
+    $csenha = $_POST['csenha'];
+    $genero = $_POST['genero'];
+    $data_inclusao = date("Y-m-d");
+    
+    // Verificar se a senha e a confirmação da senha coincidem
+    if ($senha !== $csenha) {
+        echo "A senha e a confirmação da senha não coincidem.";
+    } else {
+        // Criptografar a senha
+        $hashedPassword = password_hash($senha, PASSWORD_DEFAULT);
+
+        // Preparar e executar a consulta SQL para inserir dados
+        $stmt = $con->prepare("INSERT INTO clientes (nome, usuario, email, telefone, senha, genero,data_inclusao) VALUES (:nome, :usuario, :email, :telefone, :senha, :genero,:data_inclusao)");
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':usuario', $usuario);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':senha', $hashedPassword);
+        $stmt->bindParam(':genero', $genero);
+        $stmt->bindParam(':data_inclusao', $data_inclusao);
+
+        if ($stmt->execute()) {
+            echo "Registro bem-sucedido!";
+        } else {
+            echo "Erro ao registrar: " . $stmt->error;
+        }
+
+        // Fechar a conexão
+        $con = null;
+    }
+}
+?>
